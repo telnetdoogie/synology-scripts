170a1e14f185802c66e55e3a44a33b9e08246c06
Jason Hobbs <telnetdoogie@gmail.com>
2023-04-23 10:26:27 -0500

    Create check_certs.sh
    adding check_certs.sh which you can run to compare deployed certs to one you specify in the command arguments

---
+++ check_certs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USAGE="
+    sudo $0 /path/to/cert.pem
+"
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Please run with sudo."
+   exit 1
+fi
+
+if [[ $# -eq 0 || -z "$1" ]]; then
+  echo "Please specify the location of a cert file to compare against.
+  $USAGE "
+  exit 1
+fi
+
+CURRENT_VER=$(md5sum "$1" | awk '{print $1}')
+
+declare -a allcerts
+readarray -t allcerts < <(find /usr/syno/etc/certificate/ -type f -name "cert.pem")
+declare -a certs_unmatching
+
+if [ ${#allcerts[@]} -eq 0 ]; then
+  echo "No certificates found."
+  exit 1
+fi
+
+for dir in "${allcerts[@]}"; do
+  THIS_VERSION=$(md5sum "$dir" | awk '{print $1}')
+  if [[ "$CURRENT_VER" != "$THIS_VERSION" ]]; then
+    certs_unmatching+=("$dir")
+  fi
+done
+
+if [ ${#certs_unmatching[@]} -eq 0 ]; then
+  echo "All certificates match the current version."
+else
+  echo "The following certificates do not match the current version:"
+  printf '%s\n' "${certs_unmatching[@]}" | sed 's/^/    - /'
+fi
