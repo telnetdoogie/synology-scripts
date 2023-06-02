2c7c795e11b4bfe1d3b594d214bdc44252f23f75
Jason Hobbs <telnetdoogie@gmail.com>
2023-06-02 13:50:12 -0500

    Adding version / restore info.

--- update_docker_compose.sh
+++ update_docker_compose.sh
@@ -10,8 +10,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 if [[ ! -f "$COMPOSE_PATH/docker-compose" ]]; then
-        echo "Error: docker-compose not found in system path."
-        exit 1
+    echo "Error: docker-compose not found in system path."
+    exit 1
 fi
 
 force=0
@@ -26,22 +26,34 @@ LATEST_COMPOSE_VER=$(curl -s https://api.github.com/repos/docker/compose/release
 
 echo "Current version of docker-compose: $CURRENT_COMPOSE_VER"
 echo "Latest version of docker-compose: $LATEST_COMPOSE_VER"
+echo
 
 if [[ "$CURRENT_COMPOSE_VER" = "$LATEST_COMPOSE_VER" ]]; then
-        echo "Latest version of docker-compose already installed"
-        if [ ! "$force" -eq 1 ]; then
-                exit 1
-        else
-                echo "forcing update"
-        fi
+    echo "Latest version of docker-compose already installed!"
+    if [ ! "$force" -eq 1 ]; then
+        echo " - Exiting."
+        exit 1
+    else
+        echo " - forcing update"
+    fi
 fi
 
+echo
 echo "Installing latest version of docker-compose..."
 
+   echo "Backing up current version of docker-compose..."
+   echo "  creating backup << $COMPOSE_PATH/docker-compose.$CURRENT_COMPOSE_VER >>"
    mv $COMPOSE_PATH/docker-compose $COMPOSE_PATH/docker-compose.$CURRENT_COMPOSE_VER && \
    curl -s -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o $COMPOSE_PATH/docker-compose && \
    chmod 755 $COMPOSE_PATH/docker-compose
+   echo "  new docker-compose created"
 
 NEW_COMPOSE_VER=$(docker-compose version | sed -n -e "s/^.*version\ //p")
 
-echo "New version of docker-compose: $NEW_COMPOSE_VER"
+BLUE="\033[0;34m"
+NC="\033[0m"
+echo -e "New version of docker-compose returns: ${BLUE} ${NEW_COMPOSE_VER} ${NC}"
+echo
+echo "  to revert / replace with previous version, use the following command:"
+echo -e "  ${BLUE}sudo mv ${COMPOSE_PATH}/docker-compose.${CURRENT_COMPOSE_VER} ${COMPOSE_PATH}/docker-compose${NC}"
+echo
