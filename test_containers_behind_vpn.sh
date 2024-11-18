b3cc99b60e416cff2ce002e13882e0face867a73
Jason Hobbs <telnetdoogie@gmail.com>
2024-11-18 14:26:21 -0600

    adding test for behind-vpn containers

---
+++ test_containers_behind_vpn.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+GRN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BOLD='\033[1;4m'
+
+output(){
+	MESSAGE=$1
+	if [[ "$2" == "false" ]]; then
+		COL=$RED
+	else
+		COL=$GRN
+	fi
+	echo -e "${COL}${MESSAGE}${NC}"
+}
+
+echo
+echo "This hosts public IP:"
+PUBLIC_IP=$(wget -q -O - http://api.ipify.org)
+output "  - $PUBLIC_IP" "true"
+echo
+
+CONTAINERS=("prowlarr" "transmission")
+
+for CONTAINER in "${CONTAINERS[@]}"; do
+echo -e "Checking ${BOLD}${CONTAINER}${NC}..."
+echo " External IP: "
+EXT_IP=$(docker exec "${CONTAINER}" /usr/bin/wget -q -O - http://api.ipify.org)
+	if [[ "$EXT_IP" == "$PUBLIC_IP" ]]; then
+		output "  - ${EXT_IP}" "false"
+	else
+		output "  - ${EXT_IP}" "true"
+	fi
+	echo " Check internet egress:"
+	if docker exec "${CONTAINER}" ping -c 2 -W 1 8.8.8.8 > /dev/null 2>&1; then
+		output "  - OK" "true"
+	else
+		output "  - FAIL" "false"
+	fi
+	echo " Check DNS resolution:"
+	if docker exec "${CONTAINER}" ping -c 2 -W 1 google.com > /dev/null 2>&1; then
+		output "  - OK" "true"
+	else
+		output "  - FAIL" "false"
+	fi
+	echo
+done
+echo
