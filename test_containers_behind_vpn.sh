ddf6406691f8ce624f0016326b8b7ae38f1909e7
Jason Hobbs <telnetdoogie@gmail.com>
2024-11-23 08:59:20 -0600

    switched lack of IP to be an output fail

--- test_containers_behind_vpn.sh
+++ test_containers_behind_vpn.sh
@@ -4,7 +4,7 @@
 # No commas between items, each item in quotes.
 # eg: CONTAINERS=("prowlarr" "transmission")
 
-CONTAINERS=("prowlarr" "transmission" )
+CONTAINERS=("prowlarr" "transmission")
 
 
 # -----------------------------------------------------------
@@ -16,57 +16,66 @@ BOLD='\033[1;4m'
 
 output(){
 	MESSAGE=$1
-	if [[ "$2" == "false" ]]; then
-		COL=$RED
-	else
-		COL=$GRN
-	fi
+	COL=$GRN
+	echo -e "${COL}${MESSAGE}${NC}"
+}
+
+output_error(){
+	MESSAGE=$1
+	COL=$RED
 	echo -e "${COL}${MESSAGE}${NC}"
 }
 
 echo
 echo "This hosts public IP:"
 PUBLIC_IP=$(wget -q -O - http://api.ipify.org)
-output "  - $PUBLIC_IP" "true"
+output "  - $PUBLIC_IP"
 echo
 
 for CONTAINER in "${CONTAINERS[@]}"; do
 	echo -e "Checking ${BOLD}${CONTAINER}${NC}..."
 	echo " External IP: "
+	EXT_IP_GRABBED=false
 
 	if docker exec "${CONTAINER}" which wget >/dev/null 2>&1; then
 		EXT_IP=$(docker exec "${CONTAINER}" wget -q -O - http://api.ipify.org)
+		EXT_IP_GRABBED=true
 	elif docker exec "${CONTAINER}" which curl >/dev/null 2>&1; then
 		EXT_IP=$(docker exec "${CONTAINER}" curl -s api.ipify.org)
+		EXT_IP_GRABBED=true
 	elif docker exec "${CONTAINER}" which nc >/dev/null 2>&1; then
 		EXT_IP=$(docker exec "${CONTAINER}" sh -c "echo -e 'GET / HTTP/1.1\r\nHost: api.ipify.org\r\nConnection: close\r\n\r\n' | nc api.ipify.org 80 | grep -oE '([0-9]{1,3}\\.){3}[0-9]{1,3}'")
+		EXT_IP_GRABBED=true
 	else
-		EXT_IP="No wget,curl,nc available in container"
+		EXT_IP="\"No wget,curl,nc available in container\""
 	fi
-
-	if [[ "$EXT_IP" == "$PUBLIC_IP" ]]; then
-		output "  - ${EXT_IP}" "false"
+	if [[ $EXT_IP_GRABBED == "true" ]]; then
+		if [[ "${EXT_IP}" == "${PUBLIC_IP}" ]]; then
+			output_error "  - ${EXT_IP}"
+		else
+			output "  - ${EXT_IP}" 
+		fi
 	else
-		output "  - ${EXT_IP}" "true"
+		output_error "  - ${EXT_IP}"
 	fi
 
 	if docker exec "${CONTAINER}" which ping >/dev/null 2>&1; then
 		echo " Check internet egress:"
 		if docker exec "${CONTAINER}" ping -c 2 -W 1 8.8.8.8 > /dev/null 2>&1; then
-			output "  - OK" "true"
+			output "  - OK" 
 		else
-			output "  - FAIL" "false"
+			output_error "  - FAIL" 
 		fi
 
 		echo " Check DNS resolution:"
 		if docker exec "${CONTAINER}" ping -c 2 -W 1 google.com > /dev/null 2>&1; then
-			output "  - OK" "true"
+			output "  - OK" 
 		else
-			output "  - FAIL" "false"
+			output_error "  - FAIL"
 		fi
 	else
 		echo " Check internet egress:"
-		output "  - Ping, Name resolution check not possible, ping not availabe in container" "false"
+		output_error "  - Ping, Name resolution check not possible, ping not availabe in container" "false"
 	fi
 	echo
 done
